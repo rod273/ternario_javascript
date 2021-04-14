7fcb2f2b87702752747d1173814e5cfd0a4577aa
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-14 20:43:40 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const pontuacaoUsuario = 1999;
+const nivelUsuario =  pontuacaoUsuario >= 1000 ? 'usuario vip' : 'usuario normal';
+console.log(nivelUsuario);
+
+
+// if(pontuacaoUsuario >= 1000) {
+
+//     console.log('usuario VIP')
+
+// } else {
+//     console.log('usuario normal')
+// }
+    
